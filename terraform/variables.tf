c408ac618ec3cd85c694481341adbcd1751c7662
XaverHeuser <xaver.heuser@gmail.com>
2026-05-24 11:29:37 +0200

    change project-id

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "project_id" {
   type        = string
   description = "The ID of the project in which to create the resources."
-  default     = "finance-analysis2"
+  default     = "finance-analysis-id"
 }
 
 variable "region" {
@@ -10,13 +10,3 @@ variable "region" {
   default     = "europe-west1"
 }
 
-variable "environment" {
-  type        = string
-  description = "The environment for which to create the resources (e.g., dev, staging, prod)."
-  default     = "dev"
-
-  validation {
-    condition     = contains(["dev", "staging", "prod"], var.environment)
-    error_message = "The environment variable must be one of 'dev', 'staging', or 'prod'."
-  }
-}
