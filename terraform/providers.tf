c2896a4423dac383096d341ba2b112440babcc52
Xaver <95574334+XaverHeuser@users.noreply.github.com>
2026-06-02 08:54:51 +0200

    Fix (#111)
    * add sheets api

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -50,3 +50,9 @@ resource "google_project_service" "drive_api" {
   service            = "drive.googleapis.com"
   disable_on_destroy = false
 }
+
+resource "google_project_service" "sheets_api" {
+  project            = var.project_id
+  service            = "sheets.googleapis.com"
+  disable_on_destroy = false
+}
